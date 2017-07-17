acf20d0ec5cffe4625e54d73bcb07b58d9ebe4f2
sammayo <sammayo888@gmail.com>
2017-07-17 19:57:54 -0400

    fix nibblesToTraverse()

--- contracts/rbrelay.sol
+++ contracts/rbrelay.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
 import "./RLP.sol";
 
@@ -9,106 +9,107 @@ contract Target {
 contract rbrelay {
 	mapping(bytes32=>header) public rbchain;
 
-	uint public latest;
-	mapping(address=>bool) isSigner;
+// 	uint public latest;
+// 	mapping(address=>bool) isSigner;
 
 	struct header {
 		bytes32 parentHash;
 		bytes32 stateRoot;
 		bytes32 transactionsRoot;
+// 		bool False; // worst bug fix ever - take this out and switch to solc 0.4.13
 		bytes32 receiptsRoot;
 		uint blockNumber;
 	}
-	
-	function rbrelay() {
-	    bytes32 genesisHash = 0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177;
-	    
-	    header genesis;
-	    
-	    genesis.parentHash = 0x0;
-	    genesis.stateRoot = 0x53580584816f617295ea26c0e17641e0120cab2f0a8ffb53a866fd53aa8e8c2d;
-	    genesis.transactionsRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
-		genesis.receiptsRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
-		genesis.blockNumber = 0;
-		
-		rbchain[genesisHash] = genesis;
-		
-		latest = 0;
-
-		isSigner[0x42EB768f2244C8811C63729A21A3569731535f06] = true;
-		isSigner[0x7ffC57839B00206D1ad20c69A1981b489f772031] = true;
-		isSigner[0xB279182D99E65703F0076E4812653aaB85FCA0f0] = true;
-	}
-
-	function storeBlockHeader(bytes headerBytes, bytes32 r, bytes32 s, uint8 v) {
-		var (h, unsignedHash, blockHash) = parseBlockHeader(headerBytes,r,s,v);
-
-		require(verifyHeader(h.parentHash, unsignedHash, r, s, v));
-
-		rbchain[blockHash] = h;
-
-		latest = h.blockNumber;
-	}
-
-	function parseBlockHeader(bytes headerBytes, bytes32 r, bytes32 s, uint8 v) internal returns (header, bytes32, bytes32) 
-	{
-		RLP.RLPItem memory item = RLP.toRLPItem(headerBytes);
-        RLP.RLPItem[] memory rlpH = RLP.toList(item);
-
-        header h;
-
-		h.parentHash = RLP.toBytes32(rlpH[0]);
-		h.stateRoot = RLP.toBytes32(rlpH[3]);
-		h.transactionsRoot = RLP.toBytes32(rlpH[4]);
-		h.receiptsRoot = RLP.toBytes32(rlpH[5]);
-		h.blockNumber = RLP.toUint(rlpH[8]);
-
-		bytes32 unsignedHash = sha3(headerBytes);
-		bytes32 blockHash;
-		/*
-			block hash = sha3(RLP(h[i])) | h[12] = RLP(toData(h[12]) + r + s + v)
-		*/
-
-		return (h, unsignedHash, blockHash);
-	}
     
-	function verifyHeader(bytes32 parentHash, bytes32 unsignedHash, bytes32 r, bytes32 s, uint8 v) returns (bool) {
-	    if(rbchain[parentHash].stateRoot==0x0) {
-		    return false;
-	    }
+// 	function rbrelayInit() {
+// 	    bytes32 genesisHash = 0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177;
+	    
+// 	    header genesis;
+	    
+// 	    genesis.parentHash = 0x0;
+// 	    genesis.stateRoot = 0x53580584816f617295ea26c0e17641e0120cab2f0a8ffb53a866fd53aa8e8c2d;
+// 	    genesis.transactionsRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
+// 		genesis.receiptsRoot = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
+// 		genesis.blockNumber = 0;
 		
-		address miner = ecrecover(unsignedHash,v,r,s);
-		if (isSigner[miner]) {
-			return true;
-		}
+// 		rbchain[genesisHash] = genesis;
+		
+// 		latest = 0;
 
-		return false;
-	}
+// 		isSigner[0x42EB768f2244C8811C63729A21A3569731535f06] = true;
+// 		isSigner[0x7ffC57839B00206D1ad20c69A1981b489f772031] = true;
+// 		isSigner[0xB279182D99E65703F0076E4812653aaB85FCA0f0] = true;
+// 	}
+
+// 	function storeBlockHeader(bytes headerBytes, bytes32 r, bytes32 s, uint8 v) {
+// 		var (h, unsignedHash, blockHash) = parseBlockHeader(headerBytes,r,s,v);
+
+// 		require(verifyHeader(h.parentHash, unsignedHash, r, s, v));
+
+// 		rbchain[blockHash] = h;
+
+// 		latest = h.blockNumber;
+// 	}
+
+// 	function parseBlockHeader(bytes headerBytes, bytes32 r, bytes32 s, uint8 v) internal returns (header, bytes32, bytes32) 
+// 	{
+// 		RLP.RLPItem memory item = RLP.toRLPItem(headerBytes);
+//         RLP.RLPItem[] memory rlpH = RLP.toList(item);
+
+//         header h;
+
+// 		h.parentHash = RLP.toBytes32(rlpH[0]);
+// 		h.stateRoot = RLP.toBytes32(rlpH[3]);
+// 		h.transactionsRoot = RLP.toBytes32(rlpH[4]);
+// 		h.receiptsRoot = RLP.toBytes32(rlpH[5]);
+// 		h.blockNumber = RLP.toUint(rlpH[8]);
+
+// 		bytes32 unsignedHash = sha3(headerBytes);
+// 		bytes32 blockHash;
+// 		/*
+// 			block hash = sha3(RLP(h[i])) | h[12] = RLP(toData(h[12]) + r + s + v)
+// 		*/
+
+// 		return (h, unsignedHash, blockHash);
+// 	}
+    
+// 	function verifyHeader(bytes32 parentHash, bytes32 unsignedHash, bytes32 r, bytes32 s, uint8 v) returns (bool) {
+// 	    if(rbchain[parentHash].stateRoot==0x0) {
+// 		    return false;
+// 	    }
+		
+// 		address miner = ecrecover(unsignedHash,v,r,s);
+// 		if (isSigner[miner]) {
+// 			return true;
+// 		}
+
+// 		return false;
+// 	}
 
 	// rawTx and stack are rlp encoded
-	function relayTx(bytes rawTx, bytes txIndex, bytes stack, bytes32 blockHash, address targetAddr) {
-		bytes32 txHash = verifyTx(rawTx,txIndex,stack,blockHash);
-		require(txHash != 0x0);
+// 	function relayTx(bytes rawTx, bytes txIndex, bytes stack, bytes32 blockHash, address targetAddr) {
+// 		bytes32 txHash = verifyTx(rawTx,txIndex,stack,blockHash);
+// 		require(txHash != 0x0);
 
-		Target t = Target(targetAddr);
-		t.processTransaction(rawTx,txHash);
-	}
+// 		Target t = Target(targetAddr);
+// 		t.processTransaction(rawTx,txHash);
+// 	}
 
-	// rawTx and stack are rlp encoded
-	function verifyTx(bytes rawTx, bytes txIndex, bytes stack, bytes32 blockHash) returns (bytes32 txHash) {
-		header h = rbchain[blockHash];
-		if(h.transactionsRoot == 0x0) {
-			return 0x0;
-		}
+// 	// rawTx and stack are rlp encoded
+// 	function verifyTx(bytes rawTx, bytes txIndex, bytes stack, bytes32 blockHash) returns (bytes32 txHash) {
+// 		header h = rbchain[blockHash];
+// 		if(h.transactionsRoot == 0x0) {
+// 			return 0x0;
+// 		}
 
-		bytes32 txRoot = h.transactionsRoot;
+// 		bytes32 txRoot = h.transactionsRoot;
 
-		require(verifyMerkleProof(rawTx, txIndex, stack, txRoot));
+// 		require(verifyMerkleProof(rawTx, txIndex, stack, txRoot));
 
-		/*
-			compute and return txHash
-		*/
-	}
+// 		/*
+// 			compute and return txHash
+// 		*/
+// 	}
 
 	// value and rlpStack are rlp encoded
 	function verifyMerkleProof(bytes value, bytes path, bytes rlpStack, bytes32 root) constant returns (bool) {
@@ -152,8 +153,8 @@ contract rbrelay {
 	          	}
 	          	//extension node
           		if(nibblesToTraverse(RLP.toData(currentNodeList[0]), path, pathPtr) == 0) {
-       				return false;
-       			}
+      				return false;
+      			}
 
 				nodeKey = RLP.toBytes32(currentNodeList[1]);
 	        } else {
@@ -161,17 +162,20 @@ contract rbrelay {
 	        }
 	    }
 	}
-
-	function nibblesToTraverse(bytes encodedPartialPath, bytes path, uint pathPtr) returns (uint) {
+    
+    bytes partialPath;
+    bytes slicedPath;
+	function nibblesToTraverse(bytes encodedPartialPath, bytes path, uint pathPtr) returns (uint len) {
 		// encodedPartialPath has elements that are each two hex characters (1 byte), but partialPath
 		// and slicedPath have elements that are each one hex character (1 nibble)
 
-		bytes partialPath;
-		bytes slicedPath;
+// 		bytes partialPath;
+// 		bytes slicedPath;
 
 		uint8 hpNibble = getNthNibbleOfBytes(0,encodedPartialPath);
 		if(hpNibble == 1 || hpNibble == 3) {
-			partialPath.push(byte(getNthNibbleOfBytes(1,encodedPartialPath)));
+		    byte oddNibble = byte(getNthNibbleOfBytes(1,encodedPartialPath));
+			partialPath.push(oddNibble);
 		}
 
 		// encodedPartialPath.length is a number of bytes
@@ -182,15 +186,21 @@ contract rbrelay {
 
 		// pathPtr counts nibbles in path
 		// partialPath.length is a number of nibbles
-		for(i=pathPtr+1; i<=pathPtr+partialPath.length; i++) {
-			slicedPath.push(byte(getNthNibbleOfBytes(i,path)));
+		if(pathPtr+partialPath.length <= path.length*2-1) {
+    		for(i=pathPtr+1; i<=pathPtr+partialPath.length; i++) {
+    		    byte pathNibble = byte(getNthNibbleOfBytes(i,path));
+    			slicedPath.push(pathNibble);
+    		}
 		}
 
 		if(sha3(partialPath) == sha3(slicedPath)) {
-			return partialPath.length;
+			len = partialPath.length;
 		} else {
-			return 0;
+			len = 0;
 		}
+		
+		partialPath.length = 0;
+		slicedPath.length = 0;
 	}
 
 	function getNthNibbleOfBytes(uint n, bytes str) returns (uint8) {
--- test/verifyPrf.js
+++ test/verifyPrf.js
@@ -7,14 +7,7 @@ var ep = new proof.EthProof(new Web3.providers.HttpProvider("https://mainnet.inf
 var rbrelay = artifacts.require("./rbrelay.sol");
 
 contract('rbrelay', function(accounts) {
-  let rb
-  rbrelay.deployed().then(function(instance) {
-    rb = instance;
-  }).catch(function(e) {
-    throw new Error(e);
-  })
-  
-  it("should verify tx 0x7c9cf78f89befd42332bf13d5afb5f27f14912739c3cca9a430c11c45837ce28", function() {
+  it("should verify tx 0x7c9cf78f89befd42332bf13d5afb5f27f14912739c3cca9a430c11c45837ce28", function(done) {
     var value, path, stack, txRoot
     ep.getTxProof('0x7c9cf78f89befd42332bf13d5afb5f27f14912739c3cca9a430c11c45837ce28', function(error,result) {
       //console.log(result)
@@ -22,8 +15,16 @@ contract('rbrelay', function(accounts) {
       path = '0x' + result.path.toString('hex')
       stack = '0x' + rlp.encode(result.stack).toString('hex')
       txRoot = '0x' + result.header[4].toString('hex')
+
+      var rb
+      return rbrelay.deployed().then(function(instance) {
+        rb = instance;
+        return rb.verifyMerkleProof.call(value, path, stack, txRoot)
+      }).then(function(result) {
+        assert.isTrue(result, "merkle proof wasn't valid")
+      }).then(function() {
+        done()
+      })
     })
-    rb.verifyMerkleProof.call(value, path, stack, txRoot).then(function(result) {
-        assert.equal(result, false)
   })
 })
